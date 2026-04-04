0fb120c23f5801ec776b22cf7e96963f65b7e732
kstrekalova <strekalovakatherine@gmail.com>
2026-04-04 16:53:22 -0700

    Variables listed

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,16 @@
 # Data from https://www.kaggle.com/datasets/thedevastator/uncover-global-trends-in-mental-health-disorder
+
+# Entity: Unique identifier for each country or region included in the data set. (String)
+# Code: Unique code associated with an Entity/Country or region included in the data set. (String)
+# Year: Year that the data about that particular Entity/Country was collected. (Integer)
+# Schizophrenia (%): Percentage of people with schizophrenia in that country/region during that year. (Float)
+# Bipolar disorder (%): Percentage of people with bipolar disorder in that country/region during that year. (Float)
+# Eating disorders (%): Percentage of people with eating disorders in that country/region during that year. (Float)
+# Anxiety disorders (%): Percentage of people with anxiety disorders in that country/region during that year. (Float)
+# Drug use disorders (%): Percentage of people with drug use disorders in that country/region during that year. (Float)
+# Depression (%): Percentage of people with depression in that country/region during that year. (Float)
+# Alcohol use disorders (%): Percentage of people with alcohol use disorders in that country/region during that year. (Float)
+
+
+### Idea: correlation between disorders in country
+
